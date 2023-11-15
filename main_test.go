f3f785d59fb4e0b7ab5be08510f486145c4462b8
Ewilan Rivière <ewilan.riviere@gmail.com>
2023-11-15 19:04:38 +0100

    Add build step and update gitignore

--- main_test.go
+++ main_test.go
@@ -2,8 +2,16 @@ package main
 
 import (
 	"testing"
+
+	"github.com/ewilan-riviere/scanner/pkg/files"
+	"github.com/ewilan-riviere/scanner/pkg/mediainfo"
+	"github.com/ewilan-riviere/scanner/pkg/printer"
 )
 
 func TestWebhook(t *testing.T) {
-	//
+	items := files.Make("./")
+	printer.Print(items)
+
+	metadata := mediainfo.Make("./scanner")
+	printer.Print(metadata)
 }
